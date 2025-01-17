6b4d0cfbfdb215d00101a3922f7ec2aef58e2c61
Mike Tyler <mtyler@Mikes-MacBook-Pro-2.local>
2025-01-17 10:36:20 -0500

    resolve sensitive data issue

--- modules/monitoring/variables.tf
+++ modules/monitoring/variables.tf
@@ -2,3 +2,8 @@ variable "storage_class" {
   description = "Storage class to be used for persistent volumes"
 }
 
+variable "slack_api_url" {
+  description = "Slack API URL to be used for alertmanager. Set using TF_VAR_slack_api_url in .env"
+  sensitive = true
+}
+
